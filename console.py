f24f830c5acf4d93fd055997e3a3aad296879082
Ok-michael <okomichaelakpu2@gmail.com>
2024-02-10 17:47:19 +0100

    modify console.py

--- console.py
+++ console.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Defines the HBnB console."""
 import cmd
-from models.base_model import BaseModel
 import re
-from _parser import parse
+from shlex import split
 from models import storage
+from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
@@ -12,18 +13,33 @@ from models.amenity import Amenity
 from models.review import Review
 
 
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 
 class HBNBCommand(cmd.Cmd):
-    """
-        implement a class that inherits from the cmd class
-        and has a prompt "hbnb"
+    """Defines the HolbertonBnB command interpreter.
 
-        Attributes:
-            prompt (str): This is the command prompt.
+    Attributes:
+        prompt (str): The command prompt.
     """
-    
-    __lst = [
+
+    prompt = "(hbnb) "
+    __classes = {
         "BaseModel",
         "User",
         "State",
@@ -31,36 +47,53 @@ class HBNBCommand(cmd.Cmd):
         "Place",
         "Amenity",
         "Review"
-        ]
+    }
 
-    prompt = "(hbnb) "
-
-    def do_emptyline(self):
-        """
-            this method does nothing\n
-        """
+    def emptyline(self):
+        """Do nothing upon receiving an empty line."""
         pass
-    def do_quit(self, arg):
-        """Quit command to exit the program.\n"""
 
+    def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
         return True
 
     def do_EOF(self, arg):
-        """EOF signal to exit the program.\n"""
-
+        """EOF signal to exit the program."""
+        print("")
         return True
 
     def do_create(self, arg):
         """Usage: create <class>
         Create a new class instance and print its id.
         """
-        val = parse(arg)
-        if len(val) == 0:
+        argl = parse(arg)
+        if len(argl) == 0:
             print("** class name missing **")
-        elif val[0] not in HBNBCommand.__lst:
+        elif argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         else:
-            print(eval(val[0])().id)
+            print(eval(argl[0])().id)
             storage.save()
 
     def do_show(self, arg):
@@ -71,7 +104,7 @@ class HBNBCommand(cmd.Cmd):
         objdict = storage.all()
         if len(argl) == 0:
             print("** class name missing **")
-        elif argl[0] not in HBNBCommand.__lst:
+        elif argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         elif len(argl) == 1:
             print("** instance id missing **")
@@ -79,6 +112,7 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
         else:
             print(objdict["{}.{}".format(argl[0], argl[1])])
+
     def do_destroy(self, arg):
         """Usage: destroy <class> <id> or <class>.destroy(<id>)
         Delete a class instance of a given id."""
@@ -86,7 +120,7 @@ class HBNBCommand(cmd.Cmd):
         objdict = storage.all()
         if len(argl) == 0:
             print("** class name missing **")
-        elif argl[0] not in HBNBCommand.__lst:
+        elif argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         elif len(argl) == 1:
             print("** instance id missing **")
@@ -101,7 +135,7 @@ class HBNBCommand(cmd.Cmd):
         Display string representations of all instances of a given class.
         If no class is specified, displays all instantiated objects."""
         argl = parse(arg)
-        if len(argl) > 0 and argl[0] not in HBNBCommand.__lst:
+        if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         else:
             objl = []
@@ -134,7 +168,7 @@ class HBNBCommand(cmd.Cmd):
         if len(argl) == 0:
             print("** class name missing **")
             return False
-        if argl[0] not in HBNBCommand.__lst:
+        if argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
             return False
         if len(argl) == 1:
@@ -171,5 +205,7 @@ class HBNBCommand(cmd.Cmd):
                     obj.__dict__[k] = v
         storage.save()
 
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
+
